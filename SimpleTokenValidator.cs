0ee31a755419c19611fcc489ce5919dc345f4b94
Karpuk <dzmitry.karpuk@primetals.com>
2026-02-11 16:02:59 +0100

    add simple tocken validator

---
+++ SimpleTokenValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace UserManagementAPI;
+
+public interface ITockenValidator
+{
+    bool ValidateTocken(string tocken);
+}
+public class SimpleTokenValidator
+{ 
+    private readonly HashSet<string> _validTokens = new()
+    {
+        "token123",
+        "secret456",
+        "admin789"
+    };
+
+    public bool ValidateToken(string token)
+    {
+        return _validTokens.Contains(token);
+    }
+}
